706d38b81e467e01afe6ed61e26b1bdb8d4d86b1
kart123g <46489165+kart123g@users.noreply.github.com>
2020-12-06 21:49:22 +0530

    Add files via upload
    Upload project

---
+++ javascript pages/index.js
@@ -0,0 +1,11 @@
+new WOW().init();
+
+// FIlter anything
+$(document).ready(function () {
+  $("#anythingSearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#myDIV *").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
